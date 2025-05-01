cf63611804a0ffae5fbcae578eb66f57c182a65c
jaysi17 <jcimedenilla15@gmail.com>
2025-05-01 21:32:27 +0800

    ADDED FILES: Header, LoginPage,RegisterPage,Layout, etc

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+//ROUTES
+import Layout from './Layout.jsx';
+import IndexPage from "./pages/IndexPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from './pages/RegisterPage.jsx';
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element ={<IndexPage />} />
+        <Route path='login' element = {<LoginPage />} />
+        <Route path='register' element ={<RegisterPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
