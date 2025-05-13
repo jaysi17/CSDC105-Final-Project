1afa126aed53de4e2a0ce023afec3c0b9e875896
jaysi17 <jcimedenilla15@gmail.com>
2025-05-13 17:37:21 +0800

    ADDED CONTENTS: BOOKINGSPAGE

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import AccountNav from "../AccountNav"
 import axios from "axios"
+import PlaceImg from "../PlaceImg";
+import { differenceInCalendarDays, format } from "date-fns";
 
 export default function BookingsPage() {
     const [bookings, setBookings] = useState([]);
@@ -15,8 +17,18 @@ export default function BookingsPage() {
             <AccountNav />
             <div>
                 {bookings?.length > 0 && bookings.map(booking => (
-                    <div>
-                        {booking.checkIn} to {booking.checkOut}
+                    <div className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden ">
+                        <div className="w-48">
+                            <PlaceImg place={booking.place}/>
+                        </div>
+                        <div className="py-3">
+                            <h2>{booking.place.title}</h2>
+                            {format(new Date(booking.checkIn), 'yyyy-MM-dd')} to {format(new Date(booking.checkOut), 'yyyy-MM-dd')}
+                            <div className="border-t border-gray-300">
+                                Number of nights: {differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} <br />
+                                Total price: P{booking.price}
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
