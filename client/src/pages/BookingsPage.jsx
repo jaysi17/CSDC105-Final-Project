f0e372948730aabb25a75f6cf9a8559a51790f66
jaysi17 <jcimedenilla15@gmail.com>
2025-05-13 22:02:05 +0800

    NEW PAGE: USER BOOKINGS

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -21,13 +21,40 @@ export default function BookingsPage() {
                         <div className="w-48">
                             <PlaceImg place={booking.place}/>
                         </div>
-                        <div className="py-3">
-                            <h2>{booking.place.title}</h2>
-                            {format(new Date(booking.checkIn), 'yyyy-MM-dd')} to {format(new Date(booking.checkOut), 'yyyy-MM-dd')}
-                            <div className="border-t border-gray-300">
-                                Number of nights: {differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} <br />
-                                Total price: P{booking.price}
+                        <div className="py-3 pr-3 grow">
+                            <h2 className="text-xl">{booking.place.title}</h2>
+                            
+                            <div className="flex gap-2 border-t border-gray-300 mt-2 py-2">
+                                
                             </div>
+                            <div className="flex gap-1 text-gray-500 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                                </svg>
+                                {differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} nights:
+
+                                <div className="flex gap-1 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clipRule="evenodd" />
+                                    </svg>
+                                    {format(new Date(booking.checkIn), 'yyyy-MM-dd')}
+                                </div>
+                                
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                                </svg>
+
+                                <div className="flex gap-1 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clipRule="evenodd" />
+                                    </svg>
+                                    {format(new Date(booking.checkOut), 'yyyy-MM-dd')}
+                                </div>                                
+                            </div>
+                            <div className="text-2xl font-semibold mt-2">
+                                Total price: ₱{booking.price}
+                            </div>
+
                         </div>
                     </div>
                 ))}
