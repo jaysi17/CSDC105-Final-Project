bf04c1004cf58519268e78770b5e652cf31eada2
jaysi17 <jcimedenilla15@gmail.com>
2025-05-14 14:52:10 +0800

    CHANGED THE APP NAME TO STAYCONNECT

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
               d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
             />
           </svg>
-          <span className="shadow text-blue-100">BookThatDrive</span>
+          <span className="shadow text-blue-100">StayConnect</span>
         </Link>
 
         {/* Search bar */}
